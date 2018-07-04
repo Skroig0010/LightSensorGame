0d224d7e6cbc213b5a01b59075d4f37eac4b6dc5
Tsukasa Gotoh <go10.2ka3@ezweb.ne.jp>
2018-07-04 21:20:43 +0900

    描画の最適化
    消える壁が消えないので消えるようにする

--- app/src/main/java/jp/ac/titech/itpro/sdl/game/stage/StageMap.java
+++ app/src/main/java/jp/ac/titech/itpro/sdl/game/stage/StageMap.java
@@ -5,22 +5,22 @@ import java.util.List;
 
 import jp.ac.titech.itpro.sdl.game.entities.Entity;
 
-public class StageMap{
-    private List<List<Entity>> data;
+public class StageMap<T>{
+    private List<List<T>> data;
 
     public StageMap(int width, int height){
-        data = new ArrayList<List<Entity>>(height);
+        data = new ArrayList<List<T>>(height);
         for(int i = 0; i < height; i++){
-            data.add(new ArrayList<Entity>(width));
+            data.add(new ArrayList<T>(width));
             for(int j = 0; j < width; j++)data.get(i).add(null);
         }
     }
 
-    public Entity get(int x, int y){
+    public T get(int x, int y){
         return data.get(y).get(x);
     }
 
-    public void set(int x, int y, Entity entity){
-        data.get(y).set(x, entity);
+    public void set(int x, int y, T data){
+        this.data.get(y).set(x, data);
     }
 }
